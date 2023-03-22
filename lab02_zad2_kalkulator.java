a65daac3be129332f12b9d339ca684f1007e529e
adrian-skowronski <128103565+adrian-skowronski@users.noreply.github.com>
2023-03-22 17:33:07 +0100

    Update lab02_zad2_kalkulator.java
    Pełna wersja kalkulatora

--- lab02_zad2_kalkulator.java
+++ lab02_zad2_kalkulator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class lab02_zad2_kalkulator {
     public static void main(String[] args) {
         Run();
         Close();
@@ -11,41 +11,34 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Którą opcję wybierasz?");
         int opcja = scan.nextInt();
-        int a = InputInt1();
-        int b = InputInt2()
         switch (opcja) {
             case 1:
-                System.out.println("a + b =");
-                int a = InputInt1();
-                int b = InputInt2();
+                double a = InputInt1();
+                double b = InputInt2();
                 Suma(a, b);
                 break;
             case 2:
-                System.out.println("a - b =");
-                int a = InputInt1();
-                int b = InputInt2();
+                a = InputInt1();
+                b = InputInt2();
                 Roznica(a, b);
                 break;
             case 3:
-                System.out.println("a * b =");
-                int a = InputInt1();
-                int b = InputInt2();
+                a = InputInt1();
+                b = InputInt2();
                 Iloczyn(a, b);
                 break;
             case 4:
-                System.out.println("a / b =");
-                int a = InputInt1();
-                int b = InputInt2();
+                a = InputInt1();
+                b = InputInt2();
                 Iloraz(a, b);
                 break;
             case 5:
-                System.out.println("a ^ b =");
-                int a = InputInt1();
-                int b = InputInt2();
+                a = InputInt1();
+                b = InputInt2();
                 Potega(a, b);
+                break;
             case 6:
-                System.out.println("pierwiastek kwadratowy z a =");
-                int a = InputInt1();
+                a = InputInt1();
                 Pierwiastek(a);
                 break;
             case 7:
@@ -58,31 +51,32 @@ public class Main {
 
     }
 
-    //menu
+
     public static void Menu() {
         //wyswietlenie tekstu dla ussera z info jakie może wykonać operacje
         System.out.println("Witaj w kalkulatorze. Wybierz opcję aby wykonać przypisaną do niej operację:");
-        System.out.println("1 - suma");
-        System.out.println("2 - różnica");
-        System.out.println("3 - iloczyn");
-        System.out.println("4 - iloraz");
-        System.out.println("5 - potęga");
-        System.out.println("6 - pierwiastek");
+        System.out.println("1 - suma a+b");
+        System.out.println("2 - różnica a-b");
+        System.out.println("3 - iloczyn a*b");
+        System.out.println("4 - iloraz a/b");
+        System.out.println("5 - potęga a^b");
+        System.out.println("6 - pierwiastek kwadratowy z a");
         System.out.println("7 - funkcje trygonometryczne dla kąta podanego w mierze łukowej");
     }
 
-    public static int InputInt1() {
+    public static double InputInt1() {
         //wczytanie pierwszej liczby
         Scanner scan = new Scanner(System.in);
         System.out.println("Wprowadź liczbę a:");
-        int a = scan.nextInt();
+        double a = scan.nextDouble();
         return a;
     }
-    public static int InputInt2() {
+
+    public static double InputInt2() {
         //wczytanie drugiej liczby
         Scanner scan = new Scanner(System.in);
         System.out.print("Wprowadź liczbę b: ");
-        int b = scan.nextInt();
+        double b = scan.nextDouble();
         return b;
     }
 
@@ -92,48 +86,57 @@ public class Main {
         return znak;
     }
 
-    public static void Suma(int a, int b) {
-        int suma = Math.addExact(a, b);
-        System.out.println(a+"+"+b+"="+suma);
+    public static void Suma(double a, double b) {
+        double suma = a + b;
+        System.out.println(a + " + " + b + " = " + suma);
     }
-    public static void Roznica(int a, int b) {
-        int roznica = Math.subtractExact(a, b);
-        System.out.println(a+"-"+b+"="+roznica);
+
+    public static void Roznica(double a, double b) {
+        double roznica = a - b;
+        System.out.println(a + " - " + b + " = " + roznica);
 
     }
 
-    public static void Iloczyn(int a, int b) {
-        int iloczyn = Math.multiplyExact(a, b);
-        System.out.println(a+"*"+b+"="+iloczyn);
+    public static void Iloczyn(double a, double b) {
+        double iloczyn = a * b;
+        System.out.println(a + " * " + b + " = " + iloczyn);
     }
 
-    public static void Iloraz(int a, int b) {
-        if (b==0) System.out.println("Nie dziel przez 0!");
+    public static void Iloraz(double a, double b) {
+        if (b == 0) System.out.println("Nie dziel przez 0!");
         else {
-            int iloraz = Math.divideExact(a, b);
-            System.out.println(a + "/" + b + "=" + iloraz);
+            double iloraz = a / b;
+            System.out.println(a + " / " + b + " = " + iloraz);
         }
     }
 
-    public static void Potega(int a, int b) {
-        int potega = Math.pow(a, b);
-        System.out.println(a+"^"+b+"="+potega);
+    public static void Potega(double a, double b) {
+        double potega = Math.pow(a, b);
+        System.out.println(a + " ^ " + b + " = " + potega);
 
     }
 
-    public static void Pierwiastek(int a) {
-        int pierwiastek = Math.sqrt(a);
-        System.out.println("pierwiastek kwadratowy z "+a+"wynosi "+pierwiastek);
+    public static void Pierwiastek(double a) {
+        double pierwiastek = Math.sqrt(a);
+        System.out.println("pierwiastek kwadratowy z " + a + " wynosi " + pierwiastek);
 
     }
 
     public static void funTryg() {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj miarę łukową kąta w radianach:");
+        double kat = scan.nextDouble();
+        double sin = Math.sin(kat);
+        double cos = Math.cos(kat);
+        double tan = Math.tan(kat);
+        System.out.println("sin" + kat + " = " + sin);
+        System.out.println("cos" + kat + " = " + cos);
+        System.out.println("tan" + kat + " = " + tan);
     }
 
 
     public static void Close() {
-        System.out.println("\t\t\tCzy na pewno chcesz wyjść t || T");
+        System.out.println("\t\t\tCzy na pewno chcesz wyjść? Wybierz t lub T");
         String znak = InputStrign();
         String st1 = "t";
         String st2 = "T";
