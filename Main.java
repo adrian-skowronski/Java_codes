6b4f1b5b9c3e447761bec00e6daa421a1c9e179a
adrian-skowronski <128103565+adrian-skowronski@users.noreply.github.com>
2023-04-15 15:22:55 +0200

    lab02_zad3
    tablice

--- Main.java
+++ Main.java
@@ -1,48 +1,67 @@
 import java.util.Scanner;
 
 public class Main {
-    //7.Napisz program umożliwiający wprowadzanie n liczb
-    // oraz sortujący te liczby metodą bąbelkową lub wstawiania.
-    // Wyniki wyświetlaj na konsoli.
+    //tablica 10 intów
     public static void main(String[] args) {
-        int n = wczytajN();
-        int arr[] = tablica(n);
-        bubbleSort(arr, n);
 
-    }
-    public static int wczytajN(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Ile liczb chcesz wprowadzić?");
-                int n = scan.nextInt();
-                return n;
-    }
-    public static int[] tablica(int n){
-        int[] arr = new int[n];
-        for (int i=0; i<n; i++){
-           System.out.println("Wprowadź "+i+"."+" liczbę: ");
-           Scanner scan = new Scanner(System.in);
-           arr[i] =  scan.nextInt();
+
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Wprowadź "+i+ " element tablicy:");
+            Scanner scan = new Scanner(System.in);
+            array[i] =scan.nextInt();
         }
-        System.out.println("To już wszystkie liczby.");
-        return arr;
+        System.out.println("Co chcesz wykonać? Wybierz odpowiednią opcję:");
+        System.out.println("1 - wyświetl wszystkie elementy tablicy od pierwszego do ostatniego indeksu");
+        System.out.println("2 - wyświetl wszystkie elementy tablicy od ostatniego do pierwszego indeksu");
+        System.out.println("3 - wyświetl elementy tablicy o nieparzystych indeksach");
+        System.out.println("4 - wyświetl elementy tablicy o parzystych indeksach");
+        Scanner scanner = new Scanner(System.in);
+        int opcja;
+        opcja = scanner.nextInt();
+        switch (opcja){
+            case 1:
+                opcja1(array);
+                break;
+            case 2:
+                opcja2(array);
+                break;
+            case 3:
+                opcja3(array);
+                break;
+            case 4:
+                opcja4(array);
+                break;
+            default:
+                System.out.println("Niedostępna opcja");
+        }
     }
-
-    public static void bubbleSort(int[] arr, int n){
-
-   for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    public static void opcja1(int array[]){
+        // wyświetlanie tablicy od 1. do ostatniego elementu
+        for (int i=0; i<10; i++){
+            System.out.println("array["+i+"] ="+array[i]);
+        }
+    }
+    public static void opcja2(int array[]){
+        //wyświetlanie tablicy od ostatmiego do 1. elementu
+        for (int i=9; i>=0; i--){
+            System.out.println("array["+i+"] ="+array[i]);
+        }
+    }
+    public static void opcja3(int array[]){
+        //wyświetlanie elementów o nieparzystych indeksach
+        for (int i=0; i<10; i++){
+            if(i%2!=0){
+                System.out.println("array["+i+"] ="+array[i]);
             }
         }
     }
-
-    // Wyświetlenie posortowanych liczb
-        System.out.println("Posortowane liczby: ");
-        for (int i = 0; i < n; i++) {
-        System.out.print(arr[i] + " ");
+    public static void opcja4(int array[]){
+        //wyświetlanie elementów o parzystych indeksach
+        for (int i=0; i<10; i++){
+            if(i%2==0){
+                System.out.println("array["+i+"] ="+array[i]);
+            }
+        }
     }
-}
 }
